b0cde6929b0a4636ee1a7b5a29c689712ef0844d
bsh6463 <bsh9510@gmail.com>
2021-10-07 20:56:05 +0900

    211007 Initial Data

--- src/main/java/hello/blog/BlogApplication.java
+++ src/main/java/hello/blog/BlogApplication.java
@@ -1,14 +1,22 @@
 package hello.blog;
 
+import hello.blog.domain.comment.Comment;
+import hello.blog.domain.member.Member;
+import hello.blog.domain.post.Post;
+import hello.blog.service.comment.CommentService;
+import hello.blog.service.member.MemberService;
+import hello.blog.service.post.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
-
 
 }
---
+++ src/main/java/hello/blog/InitMember.java
@@ -0,0 +1,60 @@
+package hello.blog;
+
+import hello.blog.domain.comment.Comment;
+import hello.blog.domain.member.Member;
+import hello.blog.domain.post.Post;
+import hello.blog.service.comment.CommentService;
+import hello.blog.service.member.MemberService;
+import hello.blog.service.post.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+
+
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final MemberService memberService;
+    private final CommentService commentService;
+    private final PostService postService;
+
+
+    @PostConstruct
+    @Transactional
+    public void init(){
+
+        Member member1 = new Member("Admin", "test", "test", "test@naver.com");
+        memberService.joinMember(member1);
+
+        Post post1 = new Post("testTitle", "testContent");
+        post1.setMember(member1);
+        postService.savePost(post1);
+
+        Comment comment1 = new Comment("testComment");
+        comment1.setPost(post1);
+        comment1.setMember(member1);
+        commentService.saveComment(comment1);
+
+        Member member2 = new Member("testMember", "test1", "test1", "test1@naver.com");
+        memberService.joinMember(member2);
+
+        Post post2 = new Post("testTitle2", "testContent2");
+        post2.setMember(member2);
+        postService.savePost(post2);
+
+        Comment comment2 = new Comment("testComment2");
+        comment2.setPost(post2);
+        comment2.setMember(member2);
+        commentService.saveComment(comment2);
+
+        Comment comment3 = new Comment("testComment3");
+        comment3.setPost(post2);
+        comment3.setMember(member2);
+        commentService.saveComment(comment3);
+    }
+
+
+}
