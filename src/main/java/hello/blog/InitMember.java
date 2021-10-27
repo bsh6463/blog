0e3a3b5417222f7a532802ff4c34e610f270e3d2
bsh6463 <bsh9510@gmail.com>
2021-10-27 20:27:13 +0900

    211027 change ddl-auto

--- src/main/java/hello/blog/InitMember.java
+++ src/main/java/hello/blog/InitMember.java
@@ -23,56 +23,56 @@ public class InitMember {
     private final PostService postService;
 
 
-    @PostConstruct
-    @Transactional
-    public void init(){
-
-        Member admin = new Member("Admin", "admin", "admin", "admin@naver.com");
-        memberService.joinMember(admin);
-
-        Member member1 = new Member("test", "test", "test", "test@naver.com");
-        memberService.joinMember(member1);
-
-        Post post1 = new Post("testTitle", "testContent");
-        post1.setMember(member1);
-        postService.savePost(post1);
-
-        Comment comment1 = new Comment("testComment");
-        comment1.setPost(post1);
-        comment1.setMember(member1);
-        commentService.saveComment(comment1);
-
-        Member member2 = new Member("testMember", "test1", "test1", "test1@naver.com");
-        memberService.joinMember(member2);
-
-        Post post2 = new Post("testTitle2", "testContent2");
-        post2.setMember(member2);
-        postService.savePost(post2);
-
-        Comment comment2 = new Comment("testComment2");
-        comment2.setPost(post2);
-        comment2.setMember(member2);
-        commentService.saveComment(comment2);
-
-
-        for (int i =3;i <= 30; i++){
-            Post post = new Post("testTitle"+i, "testContent"+i);
-            post.setMember(member1);
-            postService.savePost(post);
-
-            Comment comment = new Comment("testComment"+i);
-            comment.setPost(post);
-            comment.setMember(member1);
-            commentService.saveComment(comment);
-        }
-
-
-        /**
-         * 탈퇴회원
-         */
-        Member withdrawnMember = new Member("탈퇴한 회원", "withdrawnMember", "withdrawnMember", null);
-        memberService.joinMember(withdrawnMember);
-    }
+//    @PostConstruct
+//    @Transactional
+//    public void init(){
+//
+//        Member admin = new Member("Admin", "admin", "admin", "admin@naver.com");
+//        memberService.joinMember(admin);
+//
+//        Member member1 = new Member("test", "test", "test", "test@naver.com");
+//        memberService.joinMember(member1);
+//
+//        Post post1 = new Post("testTitle", "testContent");
+//        post1.setMember(member1);
+//        postService.savePost(post1);
+//
+//        Comment comment1 = new Comment("testComment");
+//        comment1.setPost(post1);
+//        comment1.setMember(member1);
+//        commentService.saveComment(comment1);
+//
+//        Member member2 = new Member("testMember", "test1", "test1", "test1@naver.com");
+//        memberService.joinMember(member2);
+//
+//        Post post2 = new Post("testTitle2", "testContent2");
+//        post2.setMember(member2);
+//        postService.savePost(post2);
+//
+//        Comment comment2 = new Comment("testComment2");
+//        comment2.setPost(post2);
+//        comment2.setMember(member2);
+//        commentService.saveComment(comment2);
+//
+//
+//        for (int i =3;i <= 30; i++){
+//            Post post = new Post("testTitle"+i, "testContent"+i);
+//            post.setMember(member1);
+//            postService.savePost(post);
+//
+//            Comment comment = new Comment("testComment"+i);
+//            comment.setPost(post);
+//            comment.setMember(member1);
+//            commentService.saveComment(comment);
+//        }
+//
+//
+//        /**
+//         * 탈퇴회원
+//         */
+//        Member withdrawnMember = new Member("탈퇴한 회원", "withdrawnMember", "withdrawnMember", null);
+//        memberService.joinMember(withdrawnMember);
+//    }
 
 
 }
